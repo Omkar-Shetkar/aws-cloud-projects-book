7f241d54439db152c1fccdc1c41d16dc4754875f
Omkar Shetkar <omkar.shetkar@outlook.com>
2025-11-27 06:22:09 +0530

    feat: Implement authentication and recipe management features
    - Added AuthStatusCircle component to display authentication status and access token.
    - Created AuthenticatedRoute component to protect routes for authenticated users.
    - Developed CreateRecipe component for users to create new recipes with ingredients and steps.
    - Implemented RecipeItem component for displaying individual recipe items.
    - Built DisplayRecipe component to show selected recipe details and allow liking and deleting.
    - Added ListRecipes component to list all recipes with sorting functionality.
    - Integrated navigation menu with user and admin routes.
    - Configured AWS Amplify for authentication and API requests.
    - Established API interactions for fetching, creating, and deleting recipes.
    - Enhanced user experience with error handling and loading states.
    - Updated configurations for maximum ingredients, steps, and recipes.
    - Styled components using Material-UI for a consistent look and feel.

---
+++ chapter4/code/frontend/src/configs/aws-exports.ts
@@ -0,0 +1,6 @@
+export const amplifyConfig = {
+  aws_project_region: "us-east-1",
+  aws_cognito_region: "us-east-1",
+  aws_user_pools_id: "us-east-1_mrBteYTSt",
+  aws_user_pools_web_client_id: "4us1e61fpi1q3ukglmhirggap5",
+};
---
+++ chapter4/code/frontend/src/configs/configs.tsx
@@ -0,0 +1,12 @@
+export const CONFIG_MAX_INGREDIENTS = 20;
+export const CONFIG_MAX_STEPS = 10;
+export const CONFIG_MAX_RECIPES = 4;
+export const CONFIG_ADMIN_PAGE_TITLE = "Admin";
+export const CONFIG_USER_PAGE_TITLE = "User Page";
+export const appConfig = {
+  title: "CHAPTER_4",
+  iconFileName: "ch4_link.png",
+};
+
+export const API_URL =
+  "https://cyc7icn9ch.execute-api.us-east-1.amazonaws.com/dev";
